5241d10426cbd360b86415dfd3da808232de4333
floydkim <for12gm@gmail.com>
2022-10-10 21:43:56 +0900

    ignoring test

--- __tests__/CheckboxList.test.tsx
+++ __tests__/CheckboxList.test.tsx
@@ -13,27 +13,27 @@ afterEach(cleanup)
 function noop() {}
 
 // TODO: 뭘 테스트해야 의미 있을까?
+function getList(): CheckboxListItem[] {
+  return [
+    {id: 1, title: 'item1', isChecked: false},
+    {id: 2, title: 'item2', isChecked: false},
+    {id: 12, title: 'item12', isChecked: false},
+  ]
+}
 
 it('전달받은 리스트 개수만큼 Checkbox 컴포넌트를 렌더합니다', () => {
-  function getList(): CheckboxListItem[] {
-    return [
-      {id: 1, title: 'item1', isChecked: false},
-      {id: 2, title: 'item2', isChecked: false},
-      {id: 12, title: 'item12', isChecked: false},
-    ]
-  }
   const list = getList()
   render(<CheckboxList list={list} />)
   expect(screen.queryAllByText(/item*/i).length).toBe(list.length)
 })
 
-it('체크박스를 누르면 onPress 핸들러 호출', () => {
-  // TODO
-  // const handler = jest.fn()
-  // const TITLE = 'test'
-  // render(<Checkbox title={'test'} onPress={handler} />)
-  // fireEvent.press(screen.getByText(TITLE))
-  // expect(handler).toHaveBeenCalledTimes(1)
+// 이 테스트는 하위 컴포넌트에서 커버하고 있음
+xit('체크박스를 누르면 onPress 핸들러 호출', () => {
+  const list = getList()
+  render(<CheckboxList list={list} />)
+  const listItem = screen.getByText(/item/i)
+  fireEvent.press(listItem)
+  // 호출을 감지할 mock fn 전달할 prop 없음. 테스트 연습용인 이 컴포넌트는 press handler 전달받지 않을 것임.. 흠.
 })
 
 it('누른 체크박스의 체크상태가 토글되어야 함', () => {
